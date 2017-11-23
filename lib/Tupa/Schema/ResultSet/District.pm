913451605a59290ac3b7f2817b55b83da9db2cd1
Gabriel Andrade <gabiruh@gmail.com>
2017-11-23 20:52:42 -0300

    sumário

--- lib/Tupa/Schema/ResultSet/District.pm
+++ lib/Tupa/Schema/ResultSet/District.pm
@@ -11,5 +11,18 @@ use MooseX::MarkAsMethods autoclean => 1;
 extends 'DBIx::Class::ResultSet';
 with 'MyApp::Schema::Role::InflateAsHashRef';
 
+sub summary {
+  my $self = shift;
+  my $me   = $self->current_source_alias;
+  $self->search_rs(
+    undef,
+    {
+      columns => [ grep { !/geom/ } $self->result_source->columns ],
+      '+columns' => [ { geom => \'ST_AsGeoJSON(geom)' } ],
+    }
+  );
+
+}
+
 1;
 
