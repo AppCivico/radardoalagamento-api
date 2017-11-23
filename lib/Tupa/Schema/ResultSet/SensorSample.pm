27bd4a0c95d8f082fe497c9b7ad6866b4fc6b6db
Gabriel Andrade <gabiruh@gmail.com>
2017-11-23 20:52:56 -0300

    sumario

--- lib/Tupa/Schema/ResultSet/SensorSample.pm
+++ lib/Tupa/Schema/ResultSet/SensorSample.pm
@@ -16,8 +16,17 @@ sub BUILDARGS { $_[2] }
 sub last {
   my $self = shift;
   my $me   = $self->current_source_alias;
-  $self->search_rs( undef, { order_by => { -desc => "$me.event_ts" } , rows => 1} )
+  $self->search_rs( undef,
+    { order_by => { -desc => "$me.event_ts" }, rows => 1 } );
 }
 
+
+sub summary {
+  my $self = shift;
+  my $me   = $self->current_source_alias;
+  $self->search_rs( undef,
+    { order_by => { -desc => "$me.event_ts" } } );
+  
+}
 
 1;
