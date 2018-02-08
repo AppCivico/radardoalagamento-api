602a04ff145bcab40d754f1f912a7acdebfda43f
Gabriel Andrade <gabiruh@gmail.com>
2018-02-08 11:56:25 -0300

    enviando dados do usuário na resposta do `/login`

--- lib/Tupa/Web/App/Controller/Login.pm
+++ lib/Tupa/Web/App/Controller/Login.pm
@@ -6,16 +6,17 @@ use namespace::autoclean;
 BEGIN { extends 'Tupa::Web::App::Controller'; }
 
 sub base : Chained(/) PathPart(login) CaptureArgs(0) {
-  my ( $self, $c ) = @_;
+  my ($self, $c) = @_;
   $c->stash->{collection} = $c->model('DB::User');
 }
 
 sub login : Chained(base) PathPart('') Args(0) POST {
-  my ( $self, $c ) = @_;
+  my ($self, $c) = @_;
 
   my $session = $c->stash->{collection}
-    ->execute( $c, for => login => with => $c->req->data );
-  $self->status_ok( $c, entity => { api_key => $session->api_key } );
+    ->execute($c, for => login => with => $c->req->data);
+  $self->status_ok($c,
+    entity => {%{$session->user->TO_JSON}, api_key => $session->api_key});
 }
 
 __PACKAGE__->meta->make_immutable;
